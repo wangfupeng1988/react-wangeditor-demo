518f4b7907b3bf5182c806f5e1bb6dae7832e2b3
git <git@git.com>
2021-12-15 14:43:37 +0800

    u

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -6,7 +6,7 @@ function MyEditor() {
     const [editor, setEditor] = useState(null) // 存储 editor 实例
     // const defaultContent = []
     const [defaultContent, setDefaultContent] = useState([])
-    const [isAjaxDone, setIsAjaxDone] = useState(false)
+    const [isEditorShow, setIsEditorShow] = useState(false)
 
     // 模拟 ajax 请求
     setTimeout(() => {
@@ -16,27 +16,16 @@ function MyEditor() {
                 children: [{ text: "ajax 异步获取的内容" }],
             }
         ])
-        setIsAjaxDone(true)
+        setIsEditorShow(true)
     }, 1000)
 
-    const toolbarConfig = {
-        // toolbarKeys: [ /* 显示哪些菜单，如何排序、分组 */ ],
-        // excludeKeys: [ /* 隐藏哪些菜单 */ ],
-    }
+    const toolbarConfig = { }
     const editorConfig = {
         placeholder: '请输入内容...',
-        onCreated: (editor) => {
-            // 记录 editor 实例，重要 ！
-            // 有了 editor 实例，就可以执行 editor API
-            setEditor(editor)
-        },
-        onChange: (editor) => {
-            // editor 选区或者内容变化时，获取当前最新的的 content
-            console.log('changed', editor.children)
-        }
+        onCreated(editor) { setEditor(editor) }
     }
 
-    // 及时销毁 editor ，重要！！！
+    // 及时销毁 editor
     useEffect(() => {
         return () => {
             if (editor == null) return
@@ -45,31 +34,29 @@ function MyEditor() {
         }
     }, [editor])
 
-    function getEditorText() {
+    function insertText() {
         if (editor == null) return
-        console.log(editor.getText())
+        console.log(editor.insertText('hello'))
     }
 
     return (
         <>
-            <button onClick={getEditorText}>getText</button>
-            <div style={{ border: '1px solid #ccc', zIndex: 100}}>
-                {/* 渲染 toolbar */}
-                {isAjaxDone && <Toolbar
+            <button onClick={insertText}>insert text</button>
+            {isEditorShow && <div style={{ border: '1px solid #ccc', zIndex: 100}}>
+                <Toolbar
                     editor={editor}
                     defaultConfig={toolbarConfig}
                     mode="default"
                     style={{ borderBottom: '1px solid #ccc' }}
-                />}
-
-                {/* 渲染 editor */}
-                {isAjaxDone && <Editor
+                />
+                <Editor
                     defaultConfig={editorConfig}
                     defaultContent={defaultContent}
                     mode="default"
                     style={{ height: '500px' }}
-                />}
-            </div>
+                />
+            </div>}
+            {!isEditorShow && <p>loading</p>}
         </>
     )
 }
